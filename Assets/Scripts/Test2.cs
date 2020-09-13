903d4cd20d7e6853d3a87723f0090f5324b4239b
LCK <18702038509@163.com>
2020-09-13 15:15:53 +0800

    第二次更新

--- Assets/Scripts/Test2.cs
+++ Assets/Scripts/Test2.cs
@@ -2,18 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class Test2 : MonoBehaviour
 {
- 
+    private void OnEnable()
+    {
+        Debug.Log("refresh");
+    }
     // Start is called before the first frame update
     void Start()
     {
         
     }
-    
+
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Test 1");
+        
     }
 }
